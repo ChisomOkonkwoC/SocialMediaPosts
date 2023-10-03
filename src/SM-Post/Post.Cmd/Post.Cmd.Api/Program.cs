93b99158b0810cd241f048969a314bf70c9edc93
ChisomOkonkwoC <chisom.okonkwo@decagon.dev>
2023-10-03 17:19:13 +0100

    implemented the commands and commandHandler,event store and event sourcing functionalities

--- src/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ src/SM-Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,6 +1,26 @@
+using Confluent.Kafka;
+using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
+using CQRS.Core.Infrastructure;
+using CQRS.Core.Producers;
+using Post.Cmd.Api.CommandHandlers;
+using Post.Cmd.Domain.Aggregates;
+using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Handlers;
+using Post.Cmd.Infrastructure.Producers;
+using Post.Cmd.Infrastructure.Repositories;
+using Post.Cmd.Infrastructure.Stores;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
+builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection(nameof(ProducerConfig)));
+builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
+builder.Services.AddScoped<IEventProducer, EventProducer>();
+builder.Services.AddScoped<IEventStore, EventStore>();
+builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+builder.Services.AddScoped<ICommandHandler, CommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
